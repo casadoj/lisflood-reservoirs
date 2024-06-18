09c6b20a6a337efdf4342f47b9ff8f907716dc1f
casadoj <jesus.casado-rodriguez@ec.europa.eu>
2024-06-18 10:18:26 +0200

    Reorganize the reservoir models modules

---
+++ src/lisfloodreservoirs/models/__init__.py
@@ -0,0 +1,37 @@
+from .linear import Linear
+from .lisflood import Lisflood
+from .hanazaki import Hanazaki
+
+model_classes = {
+    'linear': Linear,
+    'lisflood': Lisflood,
+    'hanazaki': Hanazaki,
+}
+
+def get_model(model_name: str, *args, **kwargs):
+    """
+    Creates an instance of the specified model class.
+    
+    Parameters:
+    -----------
+    model_name: str
+        The name of the model class to instantiate.
+    *args:
+        Positional arguments to pass to the model class constructor.
+    **kwargs:
+        Keyword arguments to pass to the model class constructor.
+        
+    Returns:
+    --------
+    An instance of the specified model class.
+    """
+    
+    # Get the class from the dictionary
+    model_class = model_classes.get(model_name.lower())
+    
+    # Check if the model class exists
+    if model_class is not None:
+        # Create an instance of the model class
+        return model_class(*args, **kwargs)
+    else:
+        raise ValueError(f"Model '{model_name}' not found. Available models: {list(model_classes.keys())}")
---
+++ src/lisfloodreservoirs/models/linear.py
@@ -0,0 +1,80 @@
+import pandas as pd
+from typing import Union, List, Tuple, Dict
+
+from .basemodel import Reservoir
+
+class Linear(Reservoir):
+    """Representation of a linear reservoir"""
+    
+    def __init__(self,
+                 Vmin: float,
+                 Vtot: float,
+                 Qmin: float,
+                 T: int,
+                 At: int = 86400):
+        """        
+        Parameters:
+        -----------
+        Vmin: float
+            Volume (m3) associated to the conservative storage
+        Vtot: float
+            Total reservoir storage capacity (m3)
+        Qmin: float
+            Minimum outflow (m3/s)
+        T: int
+            Residence time in days. The coefficient of the linear reservoir is the inverse of T (1/T)
+        At: int
+            Simulation time step in seconds.
+        """
+        
+        super().__init__(Vmin, Vtot, Qmin, Qf=None, At=At)
+        
+        # storage limits
+        self.Vmin = Vmin
+        
+        # outflow limits
+        self.Qmin = Qmin
+        
+        # release coefficient
+        self.k = 1 / (T * 24 * 3600)
+        
+    def timestep(self, 
+                 I: float, 
+                 V: float
+                ) -> List[float]:
+        """Given an inflow and an initial storage values, it computes the corresponding outflow
+        
+        Parameters:
+        -----------
+        I: float
+            Inflow (m3/s)
+        V: float
+            Volume stored in the reservoir (m3)
+            
+        Returns:
+        --------
+        Q, V: List[float]
+            Outflow (m3/s) and updated storage (m3)
+        """
+        
+        # update reservoir storage with the inflow volume
+        V += I * self.At
+        
+        # ouflow depending on the inflow and storage level
+        Qmin = min(self.Qmin, (V - self.Vmin) / self.At)
+        Q = max(Qmin, V * self.k, (V - self.Vtot) / self.At)
+        
+        # update reservoir storage with the outflow volume
+        V -= Q * self.At
+        
+        return Q, V
+    
+    def get_params(self):
+        """It generates a dictionary with the reservoir paramenters in the Hanazaki model."""
+
+        params = {'Vmin': self.Vmin,
+                  'Vtot': self.Vtot,
+                  'Qmin': self.Qmin,
+                  'T': 1 / (self.k * 24 * 3600)}
+
+        return params
