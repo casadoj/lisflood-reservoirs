64b003b20d28296617e97479a6c70ce2d2dbec3f
casadoj <jesus.casado-rodriguez@ec.europa.eu>
2024-06-28 13:29:57 +0200

    Add function read_results to the calibration module

--- src/lisfloodreservoirs/calibration/__init__.py
+++ src/lisfloodreservoirs/calibration/__init__.py
@@ -1,4 +1,6 @@
-from typing import Literal
+import pandas as pd
+from typing import Literal, Union, Tuple, Dict
+from pathlib import Path
 
 from .linear import Linear_calibrator
 from .lisflood import Lisflood_calibrator
@@ -32,4 +34,33 @@ def get_calibrator(model_name: Literal['linear', 'lisflood', 'mhm'], *args, **kw
         return mHM_calibrator(*args, **kwargs)
     else:
         raise ValueError("Invalid model name. Please choose either 'linear', 'lisflood' or 'mhm'.")
+        
+        
+        
+def read_results(filename: Union[str, Path]) -> Tuple[pd.DataFrame, Dict]:
+    """It reads the CSV file resulting from the calibration, and extracts the optimal parameter set.
+    
+    Parameters:
+    -----------
+    filename: string or pathlib.Path
+        CSV file created during the calibration
+        
+    Returns:
+    --------
+    results: pandas.DataFrame
+        The data in the CSV in Pandas format
+    optimal_par: dictionary
+        Optimal parameter set, i.e., those used in the iteration with lowest likelihood
+    """
+    
+    # read results
+    results = pd.read_csv(filename)
+    results.index.name = 'iteration'
+    parcols = {col: col[3:] for col in results.columns if col.startswith('par')}
+    results.rename(columns=parcols, inplace=True)
+    
+    # extract optimal parameter set
+    optimal_par = results.iloc[results.like1.idxmin()][parcols.values()].to_dict()
+    
+    return results, optimal_par
 
