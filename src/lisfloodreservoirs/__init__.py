394d8275198cadad8fb998843d1d1ce2e07e0394
casadoj <jesus.casado-rodriguez@ec.europa.eu>
2024-07-09 17:11:35 +0200

    Update Config class to read simualtion and calibration paths

--- src/lisfloodreservoirs/__init__.py
+++ src/lisfloodreservoirs/__init__.py
@@ -18,7 +18,8 @@ class Config:
         # model configuration
         self.MODEL = self.cfg['simulation']['model'].lower()
         self.SIMULATION_CFG = self.cfg['simulation'].get('config', {})
-        self.PATH_DEF = Path(f'{self.MODEL}/default')
+        path_sim = Path(self.cfg['simulation'].get('path', './'))
+        self.PATH_DEF = path_sim / f'{self.MODEL}' / 'default'
         self.PATH_DEF.mkdir(parents=True, exist_ok=True)
         
         # calibration
@@ -26,11 +27,12 @@ class Config:
         self.TARGET = self.cfg['calibration']['target']
         self.MAX_ITER = self.cfg['calibration'].get('max_iter', 1000)
         self.COMPLEXES = self.cfg['calibration'].get('COMPLEXES', 4)
-        self.PATH_CALIB = Path('./') / self.MODEL / 'calibration'
+        path_calib = Path(self.cfg['calibration'].get('path', './'))
+        path_calib = path_calib / self.MODEL / 'calibration'
         if len(self.TARGET) == 1:
-            self.PATH_CALIB = self.PATH_CALIB / 'univariate' / self.TARGET[0]
+            self.PATH_CALIB = path_calib / 'univariate' / self.TARGET[0]
         elif len(self.TARGET) == 2:
-            self.PATH_CALIB /= 'bivariate'
+            self.PATH_CALIB = path_calib / 'bivariate'
         else:
             raise ValueError('ERROR. Only univariate or bivariate calibrations are supported')
         self.PATH_CALIB.mkdir(parents=True, exist_ok=True)
